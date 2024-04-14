19f291e7c14491dffbe443921642711af2dcb9db
appkins <nbatkins@gmail.com>
2024-04-13 20:54:04 -0500

    sort ports with protocol

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
   configs = var.swarm_mode ? var.configs : {}
 
-  ports = values({ for ports in var.ports : ports.internal => ports })
+  ports = values({ for port in var.ports : "${port.external}:${port.internal}/${port.protocol}" => port })
 }
